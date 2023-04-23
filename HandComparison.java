318357f14b5a0a7c6b0553630765358d501b76fa
tgillespie27 <tgillespie@gwu.edu>
2023-04-23 12:13:36 -0400

    commit

---
+++ HandComparison.java
@@ -0,0 +1,23 @@
+public class HandComparison {
+    public static void main (String[] args) {
+        String[] SUITS = {
+            "c", "d", "h", "s"
+        };
+
+        String[] RANKS = {
+            "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "J", "Q", "K", "A"
+        };
+        String[] table = {"2s", "5h", "7c", "10d", "Jd"};
+        String[] deck = Shuffler.shuffle();
+        String card1 = "As";
+        String card2 = "Ks";
+    }
+
+    public void HighCard() {
+        for (int i=0; i<RANKS)
+    }
+}
+    
+    
+
