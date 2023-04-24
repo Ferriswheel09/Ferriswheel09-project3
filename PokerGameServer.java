b098d36656b1bb2f46ea4a0a77c8e0ad9d22e998
Faris J <123001608+Ferriswheel09@users.noreply.github.com>
2023-04-24 19:00:30 -0400

    Fixed GUI and added sample connection demo

--- PokerGameServer.java
+++ PokerGameServer.java
@@ -17,6 +17,7 @@ public class PokerGameServer{
             index = 0;
             members = new ArrayList<String>();
             connections = new ArrayList<Socket>();
+            System.out.println("PokerServer started on port " + port);
         }
         catch(Exception e){
             System.err.println("Cannot establish server socket");
@@ -35,25 +36,24 @@ public class PokerGameServer{
 
                 //If the first line is secret, then a password is created
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSock.getInputStream()));
-                    
-                String name = in.readLine();
-                members.add(name);
+               
                         connections.add(clientSock);
 
-                        //After that, a writer is temporarily created to give the joining member all active clients currently
-                        PrintWriter pw = new PrintWriter(clientSock.getOutputStream());
-                        pw.println("START_CLIENT_LIST");
-                        for(int i=0; i<members.size(); i++){
-                            pw.println(members.get(i));
-                        }
-                        pw.println("END_CLIENT_LIST");
-                        pw.flush();
-
+                
                          //start the thread
-                        (new ClientHandler(clientSock, name)).start();
-                        (new ClientList(clientSock)).start();
-                }
-                                //continue looping
+                        (new ClientHandler(clientSock)).start();   
+
+                    if(connections.size() == 2){
+                        (new GameTracker()).start();
+                        System.out.println("GameTracker Thread Started");
+                    }
+
+               
+                
+                
+               
+                
+                //continue looping
             }catch(Exception e){} //exit serve if exception
         }
     }
@@ -61,10 +61,9 @@ public class PokerGameServer{
     private class ClientHandler extends Thread{
 
         Socket sock;
-        String name;
-        public ClientHandler(Socket sock, String name){
+
+        public ClientHandler(Socket sock){
             this.sock=sock;
-            this.name=name;
         }
 
         public void run(){
@@ -95,29 +94,8 @@ public class PokerGameServer{
 
                         }
 
-                        //After it does the removal, it sends all active connections the active list 
-                        //While removing one of the names 
-                        for(int i=0; i<connections.size(); i++){
-                            PrintWriter tempOut = new PrintWriter(connections.get(i).getOutputStream());
-                            tempOut.println("START_CLIENT_LIST");
-                            for(int j=0; j<members.size(); j++){
-                                tempOut.println(members.get(j));
-                            }
-                            tempOut.println("END_CLIENT_LIST");
-                            tempOut.flush();
-                        }
-                    }
 
-                    //Otherwise, we can assume that what we received was a message, and we can send it to all active users
-                    else{
-                    for(int i=0; i<connections.size(); i++){
-                        PrintWriter tempOut = new PrintWriter(connections.get(i).getOutputStream());
-                        tempOut.println("["+ name + "] " + msg);
-                        tempOut.flush();
-                    }
-                    }
-                    
-                    
+                    }  
                 }
 
             }catch(Exception e){}
@@ -131,64 +109,50 @@ public class PokerGameServer{
 
     public static void main(String args[]){
         int port = Integer.parseInt(args[0]);
-        GWackChannel server = new GWackChannel(port);
+        PokerGameServer server = new PokerGameServer(port);
         server.serve();
     }
 
-    //Created a separated ClientList to maintain the state of the clients
-    private class ClientList extends Thread{
-        Socket sock;
-        volatile int membersNumber;
-
-        public ClientList(Socket sock){
-            this.sock = sock;
-            membersNumber = members.size();
+ 
+    private class GameTracker extends Thread{
+        int memberNumbers;
+        boolean handshake = true;
+        boolean cardDemo = true;
+        public GameTracker(){
+            memberNumbers = 2;
         }
 
-        public synchronized void run(){
+        public void run(){
             try{
                 while(true){
-                    //If there are more members in the arraylist than the instantiated number
-                    if(membersNumber<members.size()){
-                        for(int i=0; i<connections.size(); i++){
-
-                            //We create a new connection to the output stream, and add that new member into the group
-                            PrintWriter tempOut = new PrintWriter(connections.get(i).getOutputStream());
-                            tempOut.println("START_CLIENT_LIST");
-                            for(int j=0; j<members.size(); j++){
-                                tempOut.println(members.get(j));
-                            }
-                            tempOut.println("END_CLIENT_LIST");
-                            tempOut.flush();
+                   
+                    if(handshake){
+                        for(int i=0; i<connections.size(); i++){    
+                            PrintWriter out = new PrintWriter(connections.get(i).getOutputStream());
+                            out.println("Established connection");
+                            out.flush();
+                            handshake = false;
                         }
-                        membersNumber = members.size();
-                    } 
-                        
-
-                    //If there are less members in the members arraylist, we do the same thing. 
-                    //This is for the case where a member actually leaves
-                    if(membersNumber>members.size()){
-                        for(int i=0; i<connections.size(); i++){
-                            PrintWriter tempOut = new PrintWriter(connections.get(i).getOutputStream());
-                            tempOut.println("START_CLIENT_LIST");
-                            for(int j=0; j<members.size(); j++){
-                                tempOut.println(members.get(j));
-                            }
-                            tempOut.println("END_CLIENT_LIST");
-                            tempOut.flush();
-                        }
-                        membersNumber = members.size();
                     }
-
+                    if(cardDemo){
+                        for(int i=0; i<connections.size(); i++){
+                            PrintWriter out = new PrintWriter(connections.get(i).getOutputStream());
+                            out.println("7_3");
+                            out.println("13_1");
+                            out.flush();
+                            cardDemo = false;
+                        }
+                        
+                    }
+                    
                 }
             }
-        
 
             catch(Exception e){
-                System.out.println("ClientList failed");
+                System.err.println("GameTracker failed");
             }
+
         }
-
-
     }
+    
 }
