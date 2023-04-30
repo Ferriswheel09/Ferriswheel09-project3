fbbdb4c57c9c7f2aa0a86707fc24f39f5d7052dd
tgillespie27 <tgillespie@gwu.edu>
2023-04-30 10:53:45 -0400

    ignore

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -37,67 +37,10 @@ public class DeckOfCards {
     }
 
     
-    // public static void main(String[] args) {
-    //     DeckOfCards deck = new DeckOfCards();
-    //     System.out.println("Initial deck size: " + deck.size());
-    //     deck.shuffle();
-    //     System.out.println("Shuffled deck size: " + deck.size());
 
-    //     String drawnCard = deck.drawCard();
-    //     System.out.println("Drawn card: " + drawnCard);
-    //     System.out.println("Deck size after drawing a card: " + deck.size());
-    // }
 }
 
 
 
 
 
-// import java.util.Collections;
-
-// public class Shuffle {
-
-
-//     public static String[][] shuffle() {
-//         String[][] cards = new String[4][13];
-        
-//         String[] SUITS = {
-//             "1", "2", "3", "4"
-//         };
-
-//         String[] RANKS = {
-//             "1", "2", "3", "4", "5", "6", "7", "8", "9", 
-//             "10", "11", "12", "13"
-//         };
-
-//         // initialize deck
-//         int n = SUITS.length * RANKS.length;
-//         for (int i = 0; i < SUITS.length; i++) {
-//             for (int j = 0; j < RANKS.length; j++) {
-//                 cards[i][j] = SUITS[i] + ':' + RANKS[j];
-//                 System.out.println (cards[i][j]);   
-//             }
-//         }
-//         Collenctions.shuffle(cards);
-//         return cards;
-        // for (int i = 0; i < RANKS.length; i++) {
-        //     for (int j = 0; j < SUITS.length; j++) {
-        //         System.out.println (cards[i][j]);
-                
-        //     }
-        // }
-
-        // shuffle
-        // for (int i = 0; i < n; i++) {
-        //     int r = i + (int) (Math.random() * (n-i));
-        //     String temp = deck[r];
-        //     deck[r] = deck[i];
-        //     deck[i] = temp;
-        // }
-
-        // // print shuffled deck
-        // for (int i = 0; i < n; i++) {
-//         //     System.out.println(deck[i]);
-//         // }
-//     }
-// }
